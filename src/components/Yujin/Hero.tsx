39361eb1cdb7e58da595f4a38a78626185ac50d7
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-04-29 17:17:05 +0200

    style: update yujin timeline

--- src/components/Yujin/Hero.tsx
+++ src/components/Yujin/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <div className='flex w-[1216px] flex-col items-end p-[24px] text-pink-500 md:pr-[30%]'>
                 <h1 className='text-9xl font-black uppercase'>Yujin</h1>
                 <h2 className='text-7xl font-black'>유진</h2>
-                <p className='w-full rounded-lg border-4 border-white bg-pink-500 p-4 text-right text-white lg:max-w-[500px]'>
+                <p className='w-full rounded-lg border-4 border-white bg-pink-500 p-2.5 text-center md:text-right text-white lg:max-w-[500px]'>
                     An Yu-jin, known mononymously as Yujin, is a South Korean
                     singer. She is the leader of the South Korean girl group IVE
                     under Starship Entertainment and a former member of South
--- src/components/Yujin/Timeline.tsx
+++ src/components/Yujin/Timeline.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 const Timeline = () => {
     return (
         <div
-            className='relative flex h-[80vh] items-center justify-start  bg-cover bg-center bg-no-repeat'
+            className='relative flex h-[80vh] flex-col items-center justify-start bg-cover  bg-center bg-no-repeat md:flex-row'
             style={{ backgroundImage: "url('/images/yujin-timeline.jpg')" }}
         >
             <div className='z-10 ml-[10vw] flex flex-col'>
                 <h1 className='mb-[2vw] text-center text-6xl font-black uppercase tracking-wide text-white'>
-                    Timeline
+                    <span className='text-pink-500'>Time</span>line
                 </h1>
                 <div className='i-am-gradient relative'>
                     <Image
@@ -19,34 +19,43 @@ const Timeline = () => {
                         height={45}
                         alt='Yujin I AM'
                     />
-                    <h1 className='absolute bottom-0 right-[75px] text-4xl font-black text-white'>
-                        I&apos;ve IVE
-                    </h1>
-                    <h3 className='absolute bottom-0 right-[75px] text-4xl font-black text-white'>
-                        10.04.2023
-                    </h3>
+                    <div className=''>
+                        <h1 className='absolute bottom-0 right-[75px] text-4xl font-black text-white'>
+                            I&apos;ve IVE
+                        </h1>
+                        <h3 className='absolute bottom-0 right-[75px] text-4xl font-black text-white'>
+                            10.04.2023
+                        </h3>
+                    </div>
                 </div>
             </div>
-            <div className='z-10'>
-                <h1 className='text-4xl font-black uppercase text-white'>
-                    Line distribution
-                </h1>
-                <h1 className='text-6xl font-black text-white'>263.04 s</h1>
-                <h1 className='text-2xl font-black uppercase tracking-wide text-white'>
-                    Song writing credits
-                </h1>
-                <h1 className='text-4xl font-black uppercase tracking-wide text-white'>
-                    Heroine
-                </h1>
-                <h1 className='text-xl font-black uppercase tracking-wide text-white'>
-                    Misc
-                </h1>
-                <p className='max-w-[500px] font-extralight tracking-wide text-white'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Suscipit quisquam illo minima perferendis quae error est,
-                    tempore at eius neque ex officia earum doloremque dolorem
-                    laboriosam dolorum asperiores, eveniet dignissimos!
-                </p>
+            <div className='z-10 ml-20 text-center md:text-left'>
+                <div className='mb-4'>
+                    <h1 className='text-4xl font-black uppercase text-white'>
+                        Line distribution
+                    </h1>
+                    <h1 className='text-6xl font-black text-white'>263.04 s</h1>
+                </div>
+                <div className='mb-4'>
+                    <h1 className='text-2xl font-black uppercase tracking-wide text-white'>
+                        Song writing credits
+                    </h1>
+                    <h1 className='text-4xl font-black uppercase tracking-wide text-white'>
+                        Heroine
+                    </h1>
+                </div>
+                <div className=''>
+                    <h1 className='text-xl font-black uppercase tracking-wide text-white'>
+                        Misc
+                    </h1>
+                    <p className='max-w-[500px] font-extralight tracking-wide text-white'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Suscipit quisquam illo minima perferendis quae error
+                        est, tempore at eius neque ex officia earum doloremque
+                        dolorem laboriosam dolorum asperiores, eveniet
+                        dignissimos!
+                    </p>
+                </div>
             </div>
             <div className='dotted absolute left-0 top-0 h-[100%] w-[100%] opacity-40'></div>
         </div>
