6cf8396c17c3c8dcc4a178cad999f5741433df8c
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-05-03 01:01:09 +0200

    style: fix font weight & add margin to hangul names

--- src/components/Gaeul/Hero.tsx
+++ src/components/Gaeul/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         >
             <div className='mt-[5rem] flex flex-col items-end p-[24px] text-blue-800 md:pr-[30%]'>
                 <h1 className='text-8xl font-black uppercase'>Gaeul</h1>
-                <h2 className='text-6xl'> 가을</h2>
+                <h2 className='text-6xl font-black mb-2'> 가을</h2>
                 <p className=' w-full rounded-lg border-4 border-white bg-blue-800 p-4 text-right text-white lg:max-w-[500px]'>
                     Gaeul is a South Korean rapper and singer under Starship
                     Entertainment. She is the eldest member of the girl group
--- src/components/Wonyoung/Hero.tsx
+++ src/components/Wonyoung/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <div className='flex mt-[4rem] flex-col items-end text-end p-[24px] text-red-500 md:pr-[20%]'>
                 <h1 className='text-8xl font-black uppercase sm:hidden'>Wony</h1>
                 <h1 className='text-8xl font-black uppercase hidden sm:block'>Wonyoung</h1>
-                <h2 className='text-6xl'> 원영</h2>
+                <h2 className='text-6xl font-black mb-2'> 원영</h2>
                 <p className=' w-full rounded-lg border-4 border-white bg-red-500 p-4 text-right text-white lg:max-w-[500px]'>
                     Jang Won-young, also known mononymously as Wonyoung, is a
                     South Korean singer. She is a member of Ive, a South Korean
