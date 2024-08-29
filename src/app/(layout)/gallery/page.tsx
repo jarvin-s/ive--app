c5061d017cd6188063254fda93ff6471c31a3843
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-08-29 23:52:26 +0200

    feat: add gallery (image upload & read)

--- src/app/(layout)/gallery/page.tsx
+++ src/app/(layout)/gallery/page.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+'use client'
+
+import ImageUpload from '@/components/ImageUpload'
+import { supabase } from '@/lib/supabaseClient'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 const Gallery = () => {
+    const [images, setImages] = useState<string[]>([])
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            const { data, error } = await supabase.storage
+                .from('gallery')
+                .list()
+
+            if (error) {
+                console.error('Error fetching images:', error.message)
+            } else if (data) {
+                const imageUrls = await Promise.all(
+                    data.map(async (file) => {
+                        const { data: signedData, error: signedError } =
+                            await supabase.storage
+                                .from('gallery')
+                                .createSignedUrl(file.name, 60)
+
+                        if (signedError) {
+                            console.error(
+                                'Error creating signed URL:',
+                                signedError.message
+                            )
+                        }
+                        return signedData?.signedUrl || '' // Fallback to an empty string
+                    })
+                )
+                setImages(imageUrls.filter(Boolean)) // Filter out any empty strings
+            }
+        }
+
+        fetchImages()
+    }, [])
+
     return (
-        <div className='flex h-screen items-center justify-center text-8xl'>
-            Gallery
+        <div>
+            <h1 className='flex h-[10rem] items-center justify-center text-4xl font-bold text-black'>
+                Image Gallery
+            </h1>
+            <ImageUpload />
+            <div>
+                {images.map((url, index) => (
+                    <img
+                        key={index}
+                        src={url}
+                        alt={`Image ${index + 1}`}
+                        style={{
+                            width: '150px',
+                            height: '150px',
+                            objectFit: 'cover',
+                            margin: '10px',
+                        }}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,93 @@
+import { useState, ChangeEvent } from 'react'
+import { supabase } from '../lib/supabaseClient'
+
+const ImageUpload = () => {
+    const [image, setImage] = useState<File | null>(null)
+    const [previewImage, setPreviewImage] = useState<string | null>(null)
+    const [fileName, setFileName] = useState<string | null>(null)
+
+    const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0]
+        if (file && file.type.startsWith('image/')) {
+            setFileName(file.name) // Set the file name state
+            const reader = new FileReader()
+            reader.onloadend = () => {
+                setPreviewImage(reader.result as string)
+            }
+            reader.readAsDataURL(file)
+        }
+    }
+
+    const handleImageUpload = async () => {
+        if (!image) return
+
+        const fileName = `${Date.now()}-${image.name}`
+        const { data, error } = await supabase.storage
+            .from('gallery')
+            .upload(fileName, image)
+
+        if (error) {
+            console.error('Error uploading image:', error.message)
+        } else {
+            console.log('Image uploaded successfully:', data)
+        }
+    }
+
+    return (
+        <>
+            <div className='container mx-auto flex h-full items-center justify-center p-4'>
+                <div className='w-full max-w-[350px]'>
+                    <div className='flex w-full flex-col justify-center'>
+                        <div className='upload-container relative flex w-full items-center justify-between'>
+                            <div className='w-full rounded-md border-2 border-dotted border-pink-900 text-center transition-all duration-300 hover:border-pink-600'>
+                                <label
+                                    htmlFor='file-input'
+                                    className='block h-full w-full cursor-pointer p-4 text-sm text-white'
+                                >
+                                    Drop your image here or click to browse
+                                </label>
+                                <input
+                                    name='file'
+                                    type='file'
+                                    id='file-input'
+                                    accept='image/*'
+                                    className='hidden'
+                                    onChange={handleImageChange}
+                                />
+                                <div className='preview-container hidden flex-col items-center justify-center'>
+                                    <div className='preview-image h-36 w-36 rounded-md bg-cover bg-center'></div>
+                                    <span className='file-name my-4 text-sm font-medium'></span>
+                                    <p className='close-button mb-4 cursor-pointer rounded-md border border-red-500 px-3 py-1 text-xs text-red-500 transition-all hover:bg-red-500 hover:text-white'>
+                                        Delete
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <button
+                            className='img-upload-btn mt-2'
+                            onClick={handleImageUpload}
+                        >
+                            Upload image
+                        </button>
+                        {previewImage && (
+                            <div style={{ marginTop: '20px' }}>
+                                <img
+                                    className='flex h-auto max-w-[75%] rounded-lg'
+                                    src={previewImage}
+                                    alt='Image preview'
+                                />
+                                {fileName && (
+                                    <p className='mt-[10px] font-bold'>
+                                        {fileName}
+                                    </p>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ImageUpload
