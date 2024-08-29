f5d834c9265f8ba3770d53eae58683d9a0d66882
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-08-29 23:52:37 +0200

    style: update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,54 +14,57 @@ const Navbar = () => {
     return (
         <>
             <nav
-                className={`absolute ${isClicked ? 'bg-pink-800' : 'bg-transparent'} w-full`}
+                className={`absolute ${isClicked ? 'bg-pink-900' : 'bg-transparent'} w-full`}
             >
-                <div className='bg-pink-800'>
-                    <div className='flex h-16 items-center justify-between ml-5'>
-                        <a href='/'>
+                <div className='border border-gray-500 bg-pink-900'>
+                    <div className='flex h-16 items-center justify-between'>
+                        <a className='mr-2 flex-shrink-0 object-cover' href='/'>
                             <Image src={logo} alt='IVE logo' height={45} />
                         </a>
+                        {/* <a className='mr-[24rem] lg:text-2xl font-bold text-white flex-none text-sm' href='/'>
+                            DIVE INTO IVE
+                        </a> */}
                         <div className='hidden md:block'>
-                            <div className='flex flex-row mr-[28rem] space-x-4 font-bold tracking-wide text-white'>
+                            <div className='text-sm mr-[28rem] flex font-semibold flex-row space-x-4 tracking-wide text-white'>
                                 <a
                                     href='/home'
                                     className='relative block cursor-pointer rounded-lg 
-                                    p-2  after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'
+                                    p-2 duration-200 ease-in-out hover:text-black'
                                 >
                                     Home
                                 </a>
                                 <a
                                     href='/albums'
                                     className='relative block cursor-pointer rounded-lg 
-                                    p-2  after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'
+                                    p-2 duration-200 ease-in-out hover:text-black'
                                 >
                                     Albums
                                 </a>
                                 <a
                                     href='/postcards'
                                     className='relative block cursor-pointer rounded-lg 
-                                    p-2  after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'
+                                    p-2 duration-200 ease-in-out hover:text-black'
                                 >
                                     Postcards
                                 </a>
                                 <a
                                     href='/photocards'
                                     className='relative block cursor-pointer rounded-lg 
-                                    p-2  after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'
+                                    p-2 duration-200 ease-in-out hover:text-black'
                                 >
                                     Photocards
                                 </a>
                                 <a
                                     href='/merch'
                                     className='relative block cursor-pointer rounded-lg 
-                                    p-2  after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'
+                                    p-2 duration-200 ease-in-out hover:text-black'
                                 >
                                     Merch
                                 </a>
                                 <a
                                     href='/gallery'
                                     className='relative block cursor-pointer rounded-lg 
-                                    p-2  after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'
+                                    p-2 duration-200 ease-in-out hover:text-black'
                                 >
                                     Gallery
                                 </a>
@@ -69,14 +72,14 @@ const Navbar = () => {
                         </div>
                         <div className='flex items-center md:hidden'>
                             <button
-                                className={`inline-flex items-center justify-center rounded-md p-2 ${isClicked ? 'text-white' : ''} transition duration-300 hover:bg-black  hover:text-white
+                                className={`mr-4 inline-flex items-center justify-center rounded-lg border-2 border-black p-2 ${isClicked ? 'text-white' : ''} transition duration-300 hover:bg-gray-200  hover:text-white
                             focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white`}
                                 onClick={toggleNavbar}
                             >
                                 {isClicked ? (
                                     <div>
                                         <svg
-                                            className='h-6 w-6 text-white'
+                                            className='h-6 w-6 text-gray-800'
                                             xmlns='http://www.w3.org/2000/svg'
                                             fill='none'
                                             viewBox='0 0 24 24'
@@ -93,7 +96,7 @@ const Navbar = () => {
                                 ) : (
                                     <div>
                                         <svg
-                                            className='h-6 w-6 text-white'
+                                            className='h-6 w-6 text-gray-800'
                                             xmlns='http://www.w3.org/2000/svg'
                                             fill='none'
                                             viewBox='0 0 24 24'
@@ -113,7 +116,7 @@ const Navbar = () => {
                     </div>
                 </div>
                 {isClicked && (
-                    <div className='bg-pink-800 md:hidden'>
+                    <div className='bg-pink-900 md:hidden'>
                         <div className='space-y-1 px-2 pb-4 pt-3 font-bold text-white'>
                             <a
                                 href='/home'
