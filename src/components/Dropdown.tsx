d8ae2c1ab938ddb1097b199e49e4c9ef5494f64c
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-09-03 19:04:11 +0200

    style: rework gallerly dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,85 +1,68 @@
-import { useState } from 'react'
-
-export default function Dropdown() {
-    // Define state with a boolean type
-    const [isOpen, setIsOpen] = useState<boolean>(false)
-
-    // Toggle function
-    const toggleDropdown = (): void => {
-        setIsOpen(!isOpen)
-    }
-
+const Dropdown = () => {
     return (
-        <div className='relative inline-block text-left'>
-            <button
-                id='dropdownDefaultButton'
-                onClick={toggleDropdown}
-                className='inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                type='button'
-            >
-                Gallery
+        <div className='dropdown inline-block'>
+            <button className='inline-flex items-center rounded px-4 py-2'>
+                <span className='mr-1'>Gallery</span>
                 <svg
-                    className='ms-3 h-2.5 w-2.5'
-                    aria-hidden='true'
+                    className='h-4 w-4 fill-current'
                     xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 10 6'
+                    viewBox='0 0 20 20'
                 >
-                    <path
-                        stroke='currentColor'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        d='m1 1 4 4 4-4'
-                    />
+                    <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />{' '}
                 </svg>
             </button>
-
-            {/* Dropdown menu */}
-            {isOpen && (
-                <div
-                    id='dropdown'
-                    className='absolute right-0 z-10 mt-2 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700'
-                >
-                    <ul
-                        className='py-2 text-sm text-gray-700 dark:text-gray-200'
-                        aria-labelledby='dropdownDefaultButton'
+            <ul className='dropdown-menu absolute hidden pt-1 text-gray-700'>
+                <li>
+                    <a
+                        className=' block rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400'
+                        href='/gallery/rei'
                     >
-                        <li>
-                            <a
-                                href='/gallery/rei'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                            >
-                                Rei
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='/gallery/gaeul'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                            >
-                                Gaeul
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='#'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                            >
-                                Earnings
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='#'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                            >
-                                Sign out
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            )}
+                        Rei
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className=' block rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400'
+                        href='/gallery/gaeul'
+                    >
+                        Gaeul
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className=' block rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400'
+                        href='/gallery/yujin'
+                    >
+                        Yujin
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className=' block rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400'
+                        href='/gallery/liz'
+                    >
+                        Liz
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className=' block rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400'
+                        href='/gallery/leeseo'
+                    >
+                        Leeseo
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className=' block rounded-t bg-gray-200 px-4 py-2 hover:bg-gray-400'
+                        href='/gallery/wonyoung'
+                    >
+                        Wonyoung
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
+
+export default Dropdown
