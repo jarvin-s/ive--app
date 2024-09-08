792ffadb9e14e50c928a49ca3bef755cf8c6e0db
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-09-09 00:25:11 +0200

    feat: add shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./node_modules/flowbite-react/lib/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    letterSpacing: {
-      wide: '1px',
-    },
-    extend: {},
+  	letterSpacing: {
+  		wide: '1px'
+  	},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
   plugins: [
-    require("flowbite/plugin")
-  ],
+    require("flowbite/plugin"),
+      require("tailwindcss-animate")
+],
 };
 export default config;
