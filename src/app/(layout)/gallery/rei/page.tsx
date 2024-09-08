cde80b473f07339a00daf5d651518722e202d037
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-09-09 00:25:45 +0200

    feat (WIP): add skeleton-loading component

--- src/app/(layout)/gallery/rei/page.tsx
+++ src/app/(layout)/gallery/rei/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import ImageUpload from '@/components/ImageUpload'
+import { Skeleton } from '@/components/ui/skeleton'
 import { supabase } from '@/lib/supabaseClient'
+import { setLazyProp } from 'next/dist/server/api-utils'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 const Gallery = () => {
     const [images, setImages] = useState<string[]>([])
+    const [isLoading, setLoading] = useState(true)
+    const [itemCount, setItemCount] = useState(0)
 
     useEffect(() => {
         const fetchImages = async () => {
@@ -27,14 +31,28 @@ const Gallery = () => {
                 )
                 setImages(imageUrls.filter(Boolean)) // Filter out any empty strings
             }
+            setLoading(false)
         }
 
+        const fetchImageCount = async () => {
+            const { data, error } = await supabase.storage
+                .from('gallery')
+                .list('rei/')
+
+            if (error) {
+                console.error('Error fetching items from rei/:', error.message)
+            } else {
+                setItemCount(data.length)
+            }
+        }
+
+        fetchImageCount()
         fetchImages()
     }, [])
 
     return (
         <div className='flex flex-col items-center justify-center'>
-            <div className='mx-2 mt-24 border-4 border-lime-400 bg-black p-4 sm:p-10'>
+            <div className='mx-2 mt-24 border-4 border-lime-400 bg-zinc-950 p-4 sm:p-10'>
                 <h1 className='text-center text-4xl font-bold text-white'>
                     <span className='bg-gradient-to-r from-lime-300 via-lime-400 to-lime-500 bg-clip-text text-transparent'>
                         Rei
@@ -46,22 +64,49 @@ const Gallery = () => {
                 </p>
                 <ImageUpload bucketName='rei' style='rei' />
             </div>
-            <div className='container mx-auto p-4'>
-                <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-                    {images.map((url, index) => (
-                        <Image
-                            key={index}
-                            src={url}
-                            alt={`Rei image ${index + 1}`}
-                            width={300}
-                            height={450}
-                            layout='responsive'
-                            objectFit='cover'
-                            className='gap-2 rounded-lg border-2 border-lime-400 transition duration-500 ease-in-out hover:scale-110'
-                        />
-                    ))}
+            {isLoading ? (
+                <div className='container mx-auto p-4'>
+                    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                        {images && images.length > 0 ? (
+                            <>
+                                {images.map((_, index) => (
+                                    <div
+                                        key={index}
+                                        className='flex items-center space-x-4'
+                                    >
+                                        <Skeleton className='h-12 w-12 rounded-full' />
+                                        <div className='space-y-2'>
+                                            <Skeleton className='h-4 w-[250px]' />
+                                            <Skeleton className='h-4 w-[200px]' />
+                                        </div>
+                                    </div>
+                                ))}
+                            </>
+                        ) : (
+                            <p className='text-4xl text-red-500'>
+                                No images found. Total count: {itemCount}
+                            </p>
+                        )}
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className='container mx-auto p-4'>
+                    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                        {images.map((url, index) => (
+                            <Image
+                                key={index}
+                                src={url}
+                                alt={`Rei image ${index + 1}`}
+                                width={300}
+                                height={450}
+                                layout='responsive'
+                                objectFit='cover'
+                                className='gap-2 rounded-lg border-2 border-lime-400 transition duration-500 ease-in-out hover:scale-110'
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
