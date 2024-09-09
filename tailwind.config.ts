3946638f82d2698dbbe0d2a68ba673048af354ad
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-09-09 21:15:41 +0200

    chore: fix some styling config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./node_modules/flowbite-react/lib/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	letterSpacing: {
-  		wide: '1px'
-  	},
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    letterSpacing: {
+      wide: '1px'
+    },
+    extend: {
+      colors: {}
+    }
   },
   plugins: [
     require("flowbite/plugin"),
-      require("tailwindcss-animate")
-],
+    require("tailwindcss-animate")
+  ],
 };
 export default config;
