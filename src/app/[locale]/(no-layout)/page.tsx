d29501b2045d4fd8d7e975b9988fa62369e2e42c
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-01-02 22:03:31 +0100

    style: fix animation duration 6 cards index page

--- src/app/[locale]/(no-layout)/page.tsx
+++ src/app/[locale]/(no-layout)/page.tsx
@@ -71,21 +71,21 @@ const Home = () => {
                         </a>
                         <div className='group relative ml-[5rem] w-3/12 gap-2 pt-4 md:ml-[2rem] md:w-4/12 xl:ml-[6rem]'>
                             <Image
-                                className='big-card absolute z-10  rotate-[5deg] opacity-95 duration-[400ms] ease-in-out group-hover:translate-x-[4rem] group-hover:rotate-[15deg] md:group-hover:translate-x-[10rem] md:group-hover:translate-y-12'
+                                className='big-card absolute z-10  rotate-[5deg] opacity-95 duration-300 ease-in-out group-hover:translate-x-[4rem] group-hover:rotate-[15deg] md:group-hover:translate-x-[10rem] md:group-hover:translate-y-12'
                                 src='/images/rei/rei.png'
                                 width={400}
                                 height={0}
                                 alt=''
                             />
                             <Image
-                                className='big-card absolute z-[11] opacity-95 duration-[400ms] ease-in-out group-hover:-translate-y-2 md:group-hover:translate-y-7'
+                                className='big-card absolute z-[11] opacity-95 duration-300 ease-in-out group-hover:-translate-y-2 md:group-hover:translate-y-7'
                                 src='/images/leeseo/leeseo.png'
                                 width={400}
                                 height={0}
                                 alt=''
                             />
                             <Image
-                                className='big-card absolute z-[12] rotate-[-5deg] opacity-95 duration-[400ms] ease-in-out group-hover:-translate-x-[4rem] group-hover:translate-y-2 group-hover:rotate-[-18deg] md:group-hover:-translate-x-[10rem] md:group-hover:translate-y-[3.5rem]'
+                                className='big-card absolute z-[12] rotate-[-5deg] opacity-95 duration-300 ease-in-out group-hover:-translate-x-[4rem] group-hover:translate-y-2 group-hover:rotate-[-18deg] md:group-hover:-translate-x-[10rem] md:group-hover:translate-y-[3.5rem]'
                                 src='/images/wonyoung/wonyoung.jpg'
                                 width={400}
                                 height={0}
@@ -94,21 +94,21 @@ const Home = () => {
                         </div>
                         <div className='group relative ml-[12rem] w-3/12 gap-2 md:ml-[13rem] md:w-4/12 xl:ml-[23rem]'>
                             <Image
-                                className='big-card absolute rotate-[8deg] opacity-95 duration-[400ms] ease-in-out group-hover:translate-x-[4rem] group-hover:rotate-[20deg] md:group-hover:translate-x-[7.6rem] md:group-hover:translate-y-10'
+                                className='big-card absolute rotate-[8deg] opacity-95 duration-300 ease-in-out group-hover:translate-x-[4rem] group-hover:rotate-[20deg] md:group-hover:translate-x-[7.6rem] md:group-hover:translate-y-10'
                                 src='/images/liz/liz.png'
                                 width={400}
                                 height={0}
                                 alt=''
                             />
                             <Image
-                                className='big-card absolute opacity-95 duration-[400ms] ease-in-out group-hover:-translate-y-2 md:group-hover:translate-y-5'
+                                className='big-card absolute opacity-95 duration-300 ease-in-out group-hover:-translate-y-2 md:group-hover:translate-y-5'
                                 src='/images/yujin/yujin.jpg'
                                 width={400}
                                 height={0}
                                 alt=''
                             />
                             <Image
-                                className='big-card absolute rotate-[-5deg] opacity-95 duration-[400ms] ease-in-out group-hover:z-[13] group-hover:-translate-x-[4rem] group-hover:rotate-[-12deg] md:group-hover:-translate-x-[10rem] md:group-hover:translate-y-10'
+                                className='big-card absolute rotate-[-5deg] opacity-95 duration-300 ease-in-out group-hover:z-[13] group-hover:-translate-x-[4rem] group-hover:rotate-[-12deg] md:group-hover:-translate-x-[10rem] md:group-hover:translate-y-10'
                                 src='/images/gaeul/gaeul.jpg'
                                 width={400}
                                 height={0}
