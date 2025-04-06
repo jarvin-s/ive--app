1dd920c3e27576673dd816a8b5ab4135fd736849
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-04-06 17:42:58 +0200

    feat: test opengraph image

--- src/app/[locale]/(layout)/layout.tsx
+++ src/app/[locale]/(layout)/layout.tsx
@@ -16,6 +16,13 @@ export const metadata: Metadata = {
     title: 'IVE 아이브 | DIVE INTO IVE',
     description:
         'A collection of stats, insights, and a fun quiz for IVE fans to enjoy!',
+        openGraph: {
+            images: [
+                {
+                    url: '/images/logo.png',
+                },
+            ],
+        },
 }
 
 export default async function RootLayout({
